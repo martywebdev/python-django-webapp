14d4d9094ae683802ddd9fb7d040429c1e7a37be
martywebdev <martywebdevelopment@gmail.com>
2025-10-07 22:13:15 +0800

    saving to database with flash message

--- job_application/views.py
+++ job_application/views.py
@@ -1,20 +1,17 @@
-from django.shortcuts import render
-
 from .forms import ApplicationForm
+from .models import Form
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def index(request):
-
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
-        
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            email = form.cleaned_data['email']
-            date = form.cleaned_data['date']
-            occupation = form.cleaned_data['occupation']
-        
-            print(first_name)
+            Form.objects.create(**form.cleaned_data)
+            messages.success(request, "Form submitted successfully!")
+            return redirect('index')  # ← redirect after saving
+    else:
+        form = ApplicationForm()
 
-    return render(request, template_name='index.html')
+    return render(request, 'index.html', {'form': form})
